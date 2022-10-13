e3955133442477239d27e3c87e4e619dc8fa52e6
sunhong kim <eoaud447@naver.com>
2022-10-13 12:13:41 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css'
 
 function App(){
   return(
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navigation />
       <Routes>
         <Route path='/' exact={true} element={<Home />}  />
